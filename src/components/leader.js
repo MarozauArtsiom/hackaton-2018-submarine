7e21cbeb4b73f940bc92240d23a96cd0892d81ec
Aliaksandr Akulich <Aliaksandr_Akulich@epam.com>
2018-10-20 11:57:50 +0300

    merge

--- src/components/leader.js
+++ src/components/leader.js
@@ -13,10 +13,45 @@ const list = [
   },
 ];
 
+function httpGet(url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+      const status = 200;
+      if (this.status === status) {
+        resolve(xhr.responseText);
+      } else {
+        var error = new Error(this.statusText);
+        error.code = this.status;
+        reject(error);
+      }
+    };
+    xhr.onerror = function () {
+      reject(new Error('Network Error'));
+    };
+    xhr.send();
+  });
+}
 
 
 export class LederBoard extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      leaders: []
+    };
+  }
+
+  componentDidMount() {
+    
+    console.log("componentDidMount");
+    
+    httpGet("http://192.168.43.226:6969/leaders").then((data) => this.setState({leaders: data}));
+    
+  }
+
   render() {
     return (
       <List slyle={styles.container}>
